08680cf1c5f9b44e05236f491bd974a8e7469ac1
Helpless <letalerv@gmail.com>
2025-04-09 13:13:17 +0300

    first commit

---
+++ RenderTargetCircle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using ExileCore2;
+using ExileCore2.PoEMemory.Components;
+using ExileCore2.PoEMemory.MemoryObjects;
+using ExileCore2.Shared.Enums;
+
+namespace RenderTargetCircle;
+
+public class RenderTargetCircle : BaseSettingsPlugin<RenderTargetCircleSettings>
+{
+    public override bool Initialise()
+    {
+        //Perform one-time initialization here
+
+        //Maybe load you custom config (only do so if builtin settings are inadequate for the job)
+        //var configPath = Path.Join(ConfigDirectory, "custom_config.txt");
+        //if (File.Exists(configPath))
+        //{
+        //    var data = File.ReadAllText(configPath);
+        //}
+
+        return true;
+    }
+
+    public override void AreaChange(AreaInstance area)
+    {
+        //Perform once-per-zone processing here
+        //For example, Radar builds the zone map texture here
+    }
+
+    public override void Tick()
+    {
+        //Perform non-render-related work here, e.g. position calculation.
+        //var a = Math.Sqrt(7);
+    }
+
+    public override void Render()
+    {
+        var target = GameController?.EntityListWrapper?.ValidEntitiesByType[EntityType.Player]
+            .FirstOrDefault(x => x.TryGetComponent<Player>(out var player)
+                                 && player.PlayerName.Equals(Settings.TargetName.Value,
+                                     StringComparison.CurrentCultureIgnoreCase));
+        if (target == null) return;
+        var radius = Settings.RadiusLine / 0.1f * 10;
+
+        Graphics.DrawCircleInWorld(target.Pos, radius, Settings.ColorCyrcle, Settings.ThicknessLine, 20);
+    }
+
+
+    public override void EntityAdded(Entity entity)
+    {
+        //If you have a reason to process every entity only once,
+        //this is a good place to do so.
+        //You may want to use a queue and run the actual
+        //processing (if any) inside the Tick method.
+    }
+}
---
+++ RenderTargetCircleSettings.cs
@@ -0,0 +1,21 @@
+﻿using ExileCore2.Shared.Interfaces;
+using ExileCore2.Shared.Nodes;
+
+namespace RenderTargetCircle;
+
+public class RenderTargetCircleSettings : ISettings
+{
+    public TextNode TargetName { get; set; } = new();
+    public ColorNode ColorCyrcle { get; set; } = new();
+    public RangeNode<int> ThicknessLine { get; set; } = new(1, 1, 5);
+    public RangeNode<float> RadiusLine { get; set; } = new(0, 1, 50);
+
+    public ToggleNode Enable { get; set; } = new(false);
+
+    //Mandatory setting to allow enabling/disabling your plugin
+
+    //Put all your settings here if you can.
+    //There's a bunch of ready-made setting nodes,
+    //nested menu support and even custom callbacks are supported.
+    //If you want to override DrawSettings instead, you better have a very good reason.
+}
