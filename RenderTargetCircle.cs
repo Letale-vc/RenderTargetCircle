0c09ef06bd2b65f34e0a66385d1e7fe678d4fced
Helpless <letalerv@gmail.com>
2025-04-11 20:56:05 +0300

    Refactor RenderTargetCircle logic and update settings structure

--- RenderTargetCircle.cs
+++ RenderTargetCircle.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Linq;
-using ExileCore2;
+﻿using ExileCore2;
 using ExileCore2.PoEMemory.Components;
 using ExileCore2.PoEMemory.MemoryObjects;
 using ExileCore2.Shared.Enums;
+using System;
+using System.Linq;
 
 namespace RenderTargetCircle;
 
@@ -37,12 +37,38 @@ public class RenderTargetCircle : BaseSettingsPlugin<RenderTargetCircleSettings>
 
     public override void Render()
     {
-        var target = GameController?.EntityListWrapper?.ValidEntitiesByType[EntityType.Player]
-            .FirstOrDefault(x => x.TryGetComponent<Player>(out var player)
-                                 && player.PlayerName.Equals(Settings.TargetName.Value,
-                                     StringComparison.CurrentCultureIgnoreCase));
+        Entity target = null;
+        if (Settings.TargetSelf)
+        {
+            target = GameController?.Player;
+
+        }
+        else
+        {
+            target = GameController?.EntityListWrapper?.ValidEntitiesByType[EntityType.Player]
+             .FirstOrDefault(x => x.TryGetComponent<Player>(out var player)
+                                  && player.PlayerName.Equals(Settings.TargetName.Value,
+                                      StringComparison.CurrentCultureIgnoreCase));
+        }
         if (target == null) return;
-        var radius = Settings.RadiusLine / 0.1f * 10;
+        float radius = 0f;
+        const float PER_NUMBER = 11f;
+        if (Settings.SelfPresence)
+        {
+            var stats = target.GetComponent<Stats>().StatDictionary;
+            if (stats.TryGetValue(GameStat.PresenceRadius, out var presence))
+            {
+
+                radius = (float)presence * PER_NUMBER;
+            }
+            else
+            {
+
+                LogError("Unable to find target presence");
+            }
+
+        }
+        else { radius = Settings.RadiusLine * PER_NUMBER; }
 
         Graphics.DrawCircleInWorld(target.Pos, radius, Settings.ColorCyrcle, Settings.ThicknessLine, 20);
     }
--- RenderTargetCircleSettings.cs
+++ RenderTargetCircleSettings.cs
@@ -1,14 +1,17 @@
 ﻿using ExileCore2.Shared.Interfaces;
 using ExileCore2.Shared.Nodes;
+using System.Drawing;
 
 namespace RenderTargetCircle;
 
 public class RenderTargetCircleSettings : ISettings
 {
+    public ToggleNode TargetSelf { get; set; } = new();
     public TextNode TargetName { get; set; } = new();
-    public ColorNode ColorCyrcle { get; set; } = new();
-    public RangeNode<int> ThicknessLine { get; set; } = new(1, 1, 5);
-    public RangeNode<float> RadiusLine { get; set; } = new(0, 1, 50);
+    public ColorNode ColorCyrcle { get; set; } = new(Color.FromArgb(178, 130, 230, 100));
+    public RangeNode<int> ThicknessLine { get; set; } = new(5, 1, 10);
+    public ToggleNode SelfPresence { get; set; } = new();
+    public RangeNode<int> RadiusLine { get; set; } = new(0, 1, 1000);
 
     public ToggleNode Enable { get; set; } = new(false);
 
