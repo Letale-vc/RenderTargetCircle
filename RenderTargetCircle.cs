910ffdda2417c79a1a069cd07485be125e19cf53
Helpless <letalerv@gmail.com>
2025-06-02 01:08:20 +0300

    Remove unused methods and fix color property name in RenderTargetCircle settings

--- RenderTargetCircle.cs
+++ RenderTargetCircle.cs
@@ -9,39 +9,12 @@ namespace RenderTargetCircle;
 
 public class RenderTargetCircle : BaseSettingsPlugin<RenderTargetCircleSettings>
 {
-    public override bool Initialise()
-    {
-        //Perform one-time initialization here
-
-        //Maybe load you custom config (only do so if builtin settings are inadequate for the job)
-        //var configPath = Path.Join(ConfigDirectory, "custom_config.txt");
-        //if (File.Exists(configPath))
-        //{
-        //    var data = File.ReadAllText(configPath);
-        //}
-
-        return true;
-    }
-
-    public override void AreaChange(AreaInstance area)
-    {
-        //Perform once-per-zone processing here
-        //For example, Radar builds the zone map texture here
-    }
-
-    public override void Tick()
-    {
-        //Perform non-render-related work here, e.g. position calculation.
-        //var a = Math.Sqrt(7);
-    }
-
     public override void Render()
     {
         Entity target = null;
         if (Settings.TargetSelf)
         {
             target = GameController?.Player;
-
         }
         else
         {
@@ -58,27 +31,14 @@ public class RenderTargetCircle : BaseSettingsPlugin<RenderTargetCircleSettings>
             var stats = target.GetComponent<Stats>().StatDictionary;
             if (stats.TryGetValue(GameStat.PresenceRadius, out var presence))
             {
-
                 radius = (float)presence * PER_NUMBER;
             }
             else
             {
-
                 LogError("Unable to find target presence");
             }
-
         }
         else { radius = Settings.RadiusLine * PER_NUMBER; }
-
-        Graphics.DrawCircleInWorld(target.Pos, radius, Settings.ColorCyrcle, Settings.ThicknessLine, 20);
-    }
-
-
-    public override void EntityAdded(Entity entity)
-    {
-        //If you have a reason to process every entity only once,
-        //this is a good place to do so.
-        //You may want to use a queue and run the actual
-        //processing (if any) inside the Tick method.
+        Graphics.DrawCircleInWorld(target.Pos, radius, Settings.ColorCircle, Settings.ThicknessLine, 20);
     }
 }
--- RenderTargetCircleSettings.cs
+++ RenderTargetCircleSettings.cs
@@ -8,17 +8,10 @@ public class RenderTargetCircleSettings : ISettings
 {
     public ToggleNode TargetSelf { get; set; } = new();
     public TextNode TargetName { get; set; } = new();
-    public ColorNode ColorCyrcle { get; set; } = new(Color.FromArgb(178, 130, 230, 100));
+    public ColorNode ColorCircle { get; set; } = new(Color.FromArgb(178, 130, 230, 100));
     public RangeNode<int> ThicknessLine { get; set; } = new(5, 1, 10);
     public ToggleNode SelfPresence { get; set; } = new();
     public RangeNode<int> RadiusLine { get; set; } = new(0, 1, 1000);
-
     public ToggleNode Enable { get; set; } = new(false);
 
-    //Mandatory setting to allow enabling/disabling your plugin
-
-    //Put all your settings here if you can.
-    //There's a bunch of ready-made setting nodes,
-    //nested menu support and even custom callbacks are supported.
-    //If you want to override DrawSettings instead, you better have a very good reason.
 }
